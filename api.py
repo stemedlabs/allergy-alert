1caa8e4adc6189bd7453c0724df5b73fdb866a57
Ripal Nathuji <ripal@stemedlabs.org>
2019-02-12 20:52:33 -0600

    Move call to run api

--- api.py
+++ api.py
@@ -4,8 +4,6 @@ from data_client import get_pollen_data
 # get pollen count
 
 app = Flask(__name__)
-app.run(host='127.0.0.1', port=5000)
-
 
 #get pollen count
 @app.route("/getPollenCountFromAPI", methods=['GET'])
@@ -13,6 +11,5 @@ def getPollenCount():
     pollen_data_from_data_client = get_pollen_data()
     return jsonify(pollen_data_from_data_client)
 
-
-
-
+if __name__ == "__main__":
+    app.run()
