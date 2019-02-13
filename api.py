c961a1336025e8dcb7422e54b23a5196cfaa3393
Saatvik Billa <svik724@gmail.com>
2019-02-12 19:44:03 -0600

    Adding File

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from data_client import get_pollen_data
+
+# get pollen count
+
+app = Flask(__name__)
+#app.run(host='127.0.0.1', port=5000)
+
+
+#get pollen count
+@app.route("/getPollenCountFromAPI", methods=['GET'])
+def getPollenCount():
+    pollen_data_from_data_client = get_pollen_data()
+    return jsonify(pollen_data_from_data_client)
+
+
+
+
